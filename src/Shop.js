62d07945348de145572d4817e9f8132bd41624b9
Egor Filipas <filipasegor@gmail.com>
2022-07-08 19:17:40 +0300

    LocalStorage

--- src/Shop.js
+++ src/Shop.js
@@ -1,17 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import uuid from "react-uuid";
 import ItemsList from "./ItemsList.js";
 import Item from "./Item.js";
 import AddItem from "./AddItem.js";
 
 export default function Shop() {
-  // const items = [];
 
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(() => {
+    const saved = localStorage.getItem("items");
+    if(saved){
+      return JSON.parse(saved)
+    } else{
+      return [];
+    }
+  });
+
   const [name, setName] = useState("");
   const [desc, setDesc] = useState("");
   const [validation, setValidation] = useState("");
   const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items))
+  }, [items]);
+
+
+  useEffect(() => {
+    if(items.lenght === 0){
+      document.getElementById("items-lenght").innerHTML = "Товары не добавлены"
+    }else {
+      document.getElementById("items-lenght").innerHTML = `Добавлено ${items.lenght}`
+    }
+  });
+
+  console.log(items)
 
   function handleFormSubmit(e) {
     e.preventDefault();
@@ -30,7 +52,6 @@ export default function Shop() {
       setTotal(total + 1)
     }
 
-    console.log(items);
   }
 
   function handleNameChange(e) {
@@ -55,7 +76,7 @@ export default function Shop() {
 
   const uiTotal = (
     <div>
-      <h2 className="ui-title">Всего добавлено:{total}</h2>
+      <h2 className="ui-title">Всего добавлено:</h2>
     </div>
   )
 
@@ -65,7 +86,7 @@ export default function Shop() {
       <AddItem onFormSubmit={handleFormSubmit} name={name} onNameChange={handleNameChange} onDescChange={handleDescChange} desc={desc} onButtonClick={handleFormSubmit} validation={validation}/>
 
       <ItemsList onDeleteClick={handleDeleteClick} items={items} />
-      {items.length === 0 ? !uiTotal : uiTotal}
+      <h3 id="items-lenght"></h3>
     </>
   );
 
